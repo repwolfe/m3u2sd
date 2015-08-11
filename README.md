3fc774bb58576f18463560a23ce9d2b3682c701e
repwolfe <repwolfe@gmail.com>
2015-08-11 14:36:46 +0300

    Minor fixes to the script, all working now.
    Non ASCII folder/file names were breaking things. Now using Unicode.
    
    The mp3 player I made this script for doesn't like m3u extension
    information in the playlist file, so I made it optionally added.
    
    The progress updates don't mention m3u extension lines.
    
    	modified:   README.md
    	modified:   m3u2sd.py

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # m3u2sd
-Takes a playlist file and copies its songs to an SD card, as well as an updated playlist
+Takes a playlist file and copies its songs to an SD card, and creates a new playlist with updated file locations
+
+# Running instructions
+Run the script with the following parameters:
+ - playlistPath the path to the playlist to copy to the SD card
+ - finalRoot the final root location to store the music files on the SD card
+ - (optional) saveExtensions save the m3u extension information to the new playlist (default False)
+ - (optional) replaceAll boolean if should replace existing mp3 files (default False)
--- m3u2sd.py
+++ m3u2sd.py
@@ -6,20 +6,26 @@ Created on 2015-08-11
 from __future__ import division
 import os, sys, shutil
 
+EXTENSIONS_PREFIX = "#"
 MUSIC_ROOT = "C:\\Music\\"
 SD_ROOT = "/<microSD1>/"
 
 '''
 @params playlistPath the path to the playlist to copy to the SD card
-		finalRoot the final root location to store the music files
-        replaceAll boolean if should replace existing mp3 files (optional)
+		finalRoot the final root location to store the music files on the SD card
+        (optional) saveExtensions save the m3u extension information to the new playlist (default False)
+        (optional) replaceAll boolean if should replace existing mp3 files (default False)
 '''
 if __name__ == "__main__":
     if len(sys.argv) == 3:
         playlistPath, finalRoot = sys.argv[1:]
+        saveExtensions = False
         replaceAll = False
     elif len(sys.argv) == 4:
-        playlistPath, finalRoot, replaceAll = sys.argv[1:]
+        playlistPath, finalRoot, saveExtensions = sys.argv[1:]
+        replaceAll = False
+    elif len(sys.argv) == 5:
+        playlistPath, finalRoot, saveExtensions, finalRoot = sys.argv[1:]
 
     if playlistPath == "" or finalRoot == "":
         raise Exception("Empty playlistPath or finalRoot")
@@ -32,17 +38,23 @@ if __name__ == "__main__":
 
     # Get the number of lines in the playlist for a progress bar
     numLines = 0
+    numExtensions = 0
     with open(playlistPath, "r") as f:
         for numLines, line in enumerate(f):
-            pass
+            if line[0] == EXTENSIONS_PREFIX:      # don't count m3u extension information
+                numExtensions = numExtensions + 1
 
+    numLines = numLines - numExtensions + 1    # numLines starts at 0
     lineCount = 0
     with open(playlistPath, "r") as originalPlaylist, open(newPlaylistPath, "w") as newPlaylist:
         for line in originalPlaylist:
             line = line.rstrip("\r\n")
-            if line[0] == "#":
-                # m3u extension information, simply write it to the new playlist
-                newPlaylist.write(line + "\n")
+            if line[0] == EXTENSIONS_PREFIX:
+                # m3u extension information, optionally write it to the new playlist
+                if saveExtensions:
+                    newPlaylist.write(line + "\n")
+                else:
+                    pass
             else:
                 # Extract the folder the file is in (ignoring irrelevant parent folders)
                 songLocation = os.path.relpath(line, MUSIC_ROOT)
@@ -55,11 +67,11 @@ if __name__ == "__main__":
                 finalLocation = os.path.join(finalRoot, songLocation)
                 if replaceAll or not os.path.isfile(finalLocation):
                     # Make the directory if it doesn't exist
-                    dirName = os.path.dirname(finalLocation)
+                    dirName = os.path.dirname(finalLocation).decode('utf8')
                     if not os.path.exists(dirName):
                         os.makedirs(dirName)
-                    shutil.copyfile(line, finalLocation)
+                    shutil.copyfile(line.decode('utf8'), finalLocation.decode('utf8'))
 
-            # Print progress
-            lineCount = lineCount + 1
-            print "{0}% done ({1}/{2})".format((lineCount / numLines * 100), lineCount, numLines)
+                # Print progress
+                lineCount = lineCount + 1
+                print "{0}% done ({1}/{2})".format((lineCount / numLines * 100), lineCount, numLines)
