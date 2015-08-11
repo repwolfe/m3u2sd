c68516b8bd8407541c1b02f47706871963b75863
repwolfe <repwolfe@gmail.com>
2015-08-11 11:28:27 +0300

    Main implementation complete.
    Opens playlist file
    Creates new playlist file
    Copies files from HD to SD
    Reports progress
    
    	modified:   m3u2sd.py

--- m3u2sd.py
+++ m3u2sd.py
@@ -3,19 +3,63 @@ Created on 2015-08-11
 
 @author: robbie
 '''
+from __future__ import division
+import os, sys, shutil
 
-import os, sys
+MUSIC_ROOT = "C:\\Music\\"
+SD_ROOT = "/<microSD1>/"
 
 '''
-@params startingPlaylist the path to the playlist to copy to the SD card
+@params playlistPath the path to the playlist to copy to the SD card
 		finalRoot the final root location to store the music files
+        replaceAll boolean if should replace existing mp3 files (optional)
 '''
 if __name__ == "__main__":
-    startingPlaylist, finalRoot = sys.argv[1:]
+    if len(sys.argv) == 3:
+        playlistPath, finalRoot = sys.argv[1:]
+        replaceAll = False
+    elif len(sys.argv) == 4:
+        playlistPath, finalRoot, replaceAll = sys.argv[1:]
 
-    if startingPlaylist == "" or finalRoot == "":
-        raise Exception("Empty startingPlaylist or finalRoot")
+    if playlistPath == "" or finalRoot == "":
+        raise Exception("Empty playlistPath or finalRoot")
 
-    with open(startingPlaylist, "rb") as originalPlaylist:
+    playlistName = os.path.basename(playlistPath)
+    print "Opening playlist: %s" % playlistName
+
+    newPlaylistPath = os.path.join(finalRoot, playlistName)
+    finalRootWithoutDrive = os.path.splitdrive(finalRoot)[1].lstrip(os.sep)     # Remove the drive name and slashes
+
+    # Get the number of lines in the playlist for a progress bar
+    numLines = 0
+    with open(playlistPath, "r") as f:
+        for numLines, line in enumerate(f):
+            pass
+
+    lineCount = 0
+    with open(playlistPath, "r") as originalPlaylist, open(newPlaylistPath, "w") as newPlaylist:
         for line in originalPlaylist:
-            print line
+            line = line.rstrip("\r\n")
+            if line[0] == "#":
+                # m3u extension information, simply write it to the new playlist
+                newPlaylist.write(line + "\n")
+            else:
+                # Extract the folder the file is in (ignoring irrelevant parent folders)
+                songLocation = os.path.relpath(line, MUSIC_ROOT)
+
+                # Write to the new playlist file
+                finalLocation = os.path.join(SD_ROOT, finalRootWithoutDrive, songLocation).replace("\\", "/")
+                newPlaylist.write(finalLocation + "\n")
+
+                # Copy the file to its new location (if it doesn't exist)
+                finalLocation = os.path.join(finalRoot, songLocation)
+                if replaceAll or not os.path.isfile(finalLocation):
+                    # Make the directory if it doesn't exist
+                    dirName = os.path.dirname(finalLocation)
+                    if not os.path.exists(dirName):
+                        os.makedirs(dirName)
+                    shutil.copyfile(line, finalLocation)
+
+            # Print progress
+            lineCount = lineCount + 1
+            print "{0}% done ({1}/{2})".format((lineCount / numLines * 100), lineCount, numLines)
